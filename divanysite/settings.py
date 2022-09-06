d2346bd2f905a8d626338973c5aa03a572764608
divanyh <100296688+divanyh@users.noreply.github.com>
2022-09-07 00:43:07 +0700

    Update settings.py

--- divanysite/settings.py
+++ divanysite/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
